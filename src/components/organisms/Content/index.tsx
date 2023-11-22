7a08bf8bd1ec2c7b2169e80943e1a2b8458b4a57
KM0912 <61630275+KM0912@users.noreply.github.com>
2023-11-23 07:46:21 +0900

    Add textarea for user notes in Content component

--- src/components/organisms/Content/index.tsx
+++ src/components/organisms/Content/index.tsx
@@ -1,20 +1,31 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 import { Layout, theme } from "antd";
-import React from "react";
+import TextArea from "antd/es/input/TextArea";
 
 const { Content: AntContent } = Layout;
 
+const SDiv = styled.div`
+  padding: 24px;
+  background: ${(props) => props.theme.colorBgContainer};
+`;
+
 const Content: React.FC = () => {
+  const [value, setValue] = useState("");
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
   return (
     <AntContent className="site-layout">
-      <div
-        style={{ padding: 24, minHeight: 380, background: colorBgContainer }}
-      >
-        Content
-      </div>
+      <SDiv style={{ padding: 24, background: colorBgContainer }}>
+        <TextArea
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter your notes here..."
+          autoSize={{ minRows: 25, maxRows: 25 }}
+        />
+      </SDiv>
     </AntContent>
   );
 };
